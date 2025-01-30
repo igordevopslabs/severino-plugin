20d823e693628973e83550d3b66a99906d72962c
igoritos22 <igoritos_22@hotmail.com>
2025-01-30 19:04:16 -0300

    feat: add doc for testes

--- generate-token/go.mod
+++ generate-token/go.mod
@@ -2,4 +2,7 @@ module github.com/igordevopslabs/generate-token
 
 go 1.22.5
 
-require github.com/golang-jwt/jwt v3.2.2+incompatible
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/golang-jwt/jwt/v4 v4.5.1
+)
--- generate-token/main.go
+++ generate-token/main.go
@@ -3,37 +3,34 @@ package main
 import (
 	"fmt"
 
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v4"
 )
 
-var (
-	JWT_SECRET = "mysecret@12345"
-)
+func GenerateTokenJwt() (string, error) {
+	SECRET_KEY := "teste"
+
+	claims := jwt.MapClaims{
+		"iss":   "my-issuer",
+		"roles": []string{"admin", "batata"},
+	}
+
+	// Criar token com header fixo
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token.Header["alg"] = "HS256" // Garantir que o header é idêntico
+	token.Header["typ"] = "JWT"
+
+	tokenString, err := token.SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
 
 func main() {
 	token, err := GenerateTokenJwt()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Erro ao gerar token:", err)
+		return
 	}
-	fmt.Printf("TOKEN: %s\n", token)
-
-}
-
-func GenerateTokenJwt() (string, error) {
-	// Create a new token object, specifying signing method and the claims
-	claims := jwt.MapClaims{
-		"iss":   "my-issuer",
-		"roles": []string{"admin"},
-		//"exp":   time.Now().Add(time.Hour * 1).Unix(),
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString([]byte(JWT_SECRET))
-
-	if err != nil {
-		fmt.Println(err)
-		return "", err
-	}
-
-	return tokenString, nil
+	fmt.Println(token)
 }
